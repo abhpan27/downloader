9a62ed3f9c224a229d233d4c955a9c06f3fc8c40
Abhishek Pandey <abhishekpandey@Abhisheks-MacBook-Pro.local>
2017-03-18 10:55:20 +0530

    Initial Commit

---
+++ iDownloader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iDownloader
+//
+//  Created by Abhishek Pandey on 18/03/17.
+//  Copyright © 2017 Abhishek Pandey. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
