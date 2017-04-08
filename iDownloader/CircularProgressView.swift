f3f00aae047f04c12472eb6fa1653607ae1e42c1
Abhishek Pandey <abhishekpandey@abhisheks-macbook-pro.local>
2017-04-08 15:18:03 +0530

    Removed percent layer from circular list.

--- iDownloader/CircularProgressView.swift
+++ iDownloader/CircularProgressView.swift
@@ -14,7 +14,7 @@ open class CircularProgressView: DeterminateAnimation {
 
     var backgroundCircle = CAShapeLayer()
     var progressLayer = CAShapeLayer()
-    var percentLabelLayer = CATextLayer()
+//    var percentLabelLayer = CATextLayer()
 
     @IBInspectable open var strokeWidth: CGFloat = -1 {
         didSet {
@@ -32,11 +32,11 @@ open class CircularProgressView: DeterminateAnimation {
         super.notifyViewRedesigned()
         backgroundCircle.lineWidth = self.strokeWidth / 8
         progressLayer.lineWidth = strokeWidth
-        percentLabelLayer.isHidden = !showPercent
+        //percentLabelLayer.isHidden = !showPercent
 
         backgroundCircle.strokeColor = foreground.withAlphaComponent(0.5).cgColor
         progressLayer.strokeColor = foreground.cgColor
-        percentLabelLayer.foregroundColor = foreground.cgColor
+        //percentLabelLayer.foregroundColor = foreground.cgColor
     }
 
     override func updateProgress() {
@@ -49,7 +49,7 @@ open class CircularProgressView: DeterminateAnimation {
         let timing = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
         CATransaction.setAnimationTimingFunction(timing)
         progressLayer.strokeEnd = max(0, min(progress, 1))
-        percentLabelLayer.string = "\(Int(progress * 100))%"
+        //percentLabelLayer.string = "\(Int(progress * 100))%" // no need to show percent layer
         CATransaction.commit()
     }
 
@@ -90,15 +90,15 @@ open class CircularProgressView: DeterminateAnimation {
         }
 
         // Percentage Layer
-        do {
-            percentLabelLayer.string = "0%"
-            percentLabelLayer.foregroundColor = foreground.cgColor
-            percentLabelLayer.frame = rect
-            percentLabelLayer.font = "Helvetica Neue Light" as CFTypeRef
-            percentLabelLayer.alignmentMode = kCAAlignmentCenter
-            percentLabelLayer.position.y = rect.midY * 0.25
-            percentLabelLayer.fontSize = rect.width * 0.2
-            self.layer?.addSublayer(percentLabelLayer)
-        }
+//        do {
+//            percentLabelLayer.string = "0%"
+//            percentLabelLayer.foregroundColor = foreground.cgColor
+//            percentLabelLayer.frame = rect
+//            percentLabelLayer.font = "Helvetica Neue Light" as CFTypeRef
+//            percentLabelLayer.alignmentMode = kCAAlignmentCenter
+//            percentLabelLayer.position.y = rect.midY * 0.25
+//            percentLabelLayer.fontSize = rect.width * 0.2
+//            self.layer?.addSublayer(percentLabelLayer)
+//        }
     }
 }
